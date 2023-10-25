8fb2685f35b9616256569b7c526d83e074d1ca6e
SSayak <delicious_peach@mail.ru>
2023-10-25 15:58:33 +0600

    Games

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,20 @@ import "./App.scss";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Netflix from "./pages/Netflix";
+import Games from "./games/Games";
+import Cards from "./games/Cards";
 
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route exact path="/login" element={<Login />} />
-      <Route exact path="/signup" element={<Signup />} />
-      <Route exact path="/" element={<Netflix />} />
-    </Routes>
+      <Routes>
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/signup" element={<Signup />} />
+        <Route exact path="/" element={<Netflix />} />
+        <Route exact path="/games" element={<Games />} />
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
 export default App;
