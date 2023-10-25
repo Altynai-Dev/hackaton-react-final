2d31f28d515f3b3362cb4d87c1a6be405ecb621e
Asel <nurubekovna@gmail.com>
2023-10-25 18:56:48 +0600

    first commit asel

--- src/pages/Netflix.jsx
+++ src/pages/Netflix.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React , { useState } from 'react'
+import Navbar from "../components/Navbar"
 
 const Netflix = () => {
+    const [isScrolled, setIsScrolled] = useState(false);
+  window.onscroll = () => {
+    setIsScrolled(window.pageYOffset === 0 ? false : true);
+    return () => (window.onscroll = null);
+  };
+
   return (
-    <h1>сделать update token</h1>
+  <Navbar isScrolled={isScrolled}/>
   )
 }
 
