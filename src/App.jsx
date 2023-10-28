bd7fe1e72c3fb0f37e25665f00a50b0c2752ba3b
Daniyarow <danieldaniyarow@gmail.com>
2023-10-28 17:48:14 +1200

    Song Player

--- src/App.jsx
+++ src/App.jsx
@@ -6,31 +6,83 @@ import Netflix from "./pages/Netflix/Netflix";
 import Games from "./components/games/Games";
 import Cards from "./components/games/Cards";
 import Player from "./pages/PlayerPage/Player";
+import SongPlayer from "./components/SongPlayer/SongPlayer";
 import { Provider } from "react-redux";
 import store from "./store/store";
 import Details from "./pages/Details/Details";
 import Seasons from "./pages/Seasons/Seasons";
 import Series from "./pages/Series/Series";
 import AnimeGame from "./components/games/AnimeGame/Animegame";
-
+import { useEffect, useState } from "react";
 
 function App() {
+  const [songs] = useState([
+    {
+      title: "Forget me too ft. Halsey",
+      artist: "Machine Gun Kelly",
+      img_src: "./images/song-1.jpg",
+      src: "./music/on-n-on.mp3",
+    },
+    {
+      title: "Song 2",
+      artist: "Artist 2",
+      img_src: "./images/song-2.jpg",
+      src: "./music/somebody-new.mp3",
+    },
+    {
+      title: "Song 3",
+      artist: "Artist 3",
+      img_src: "./images/song-3.jpg",
+      src: "./music/on-n-on.mp3",
+    },
+    {
+      title: "Song 4",
+      artist: "Artist 4",
+      img_src: "./images/song-4.jpg",
+      src: "./music/somebody-new.mp3",
+    },
+  ]);
+
+  const [currentSongIndex, setCurrentSongIndex] = useState(0);
+  const [nextSongIndex, setNextSongIndex] = useState(0);
+
+  useEffect (() => {
+    setNextSongIndex(() => {
+      if (currentSongIndex + 1 > songs.length - 1) {
+        return 0;
+      } else {
+        return currentSongIndex + 1;
+      }
+    });
+  }, [currentSongIndex]);
+  
   return (
     <Provider store={store}>
-    <BrowserRouter>
-    <Routes>
-      <Route exact path="/login" element={<Login />} />
-      <Route exact path="/signup" element={<Signup />} />
-      <Route exact path="/player" element={<Player/>}/>
-      <Route exact path="/" element={<Netflix />} />
-      <Route exact path="/games" element={<Games />} />
-      <Route path="/series/:slug" element={<Details />} />
-      <Route path="/series/:slug/series" element={<Seasons />} />
-      <Route path="/series/watch/:slug" element={<Series />} />
-      <Route path="/animegame" element={<AnimeGame />} />
-
-    </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/signup" element={<Signup />} />
+          <Route exact path="/player" element={<Player />} />
+          <Route
+            exact
+            path="/song"
+            element={
+              <SongPlayer
+                currentSongIndex={currentSongIndex}
+                setCurrentSongIndex={setCurrentSongIndex}
+                nextSongIndex={nextSongIndex}
+                songs={songs}
+              />
+            }
+          />
+          <Route exact path="/" element={<Netflix />} />
+          <Route exact path="/games" element={<Games />} />
+          <Route path="/series/:slug" element={<Details />} />
+          <Route path="/series/:slug/series" element={<Seasons />} />
+          <Route path="/series/watch/:slug" element={<Series />} />
+          <Route path="/animegame" element={<AnimeGame />} />
+        </Routes>
+      </BrowserRouter>
     </Provider>
   );
 }
