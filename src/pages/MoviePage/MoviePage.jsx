45c94b749f2ae28b9110a5b574164c152a219de8
Asel <nurubekovna@gmail.com>
2023-11-02 11:22:53 +0600

    adap

--- src/pages/MoviePage/MoviePage.jsx
+++ src/pages/MoviePage/MoviePage.jsx
@@ -1,25 +1,21 @@
 import React from "react";
 import Navbar from "../../components/navbar/Navbar";
-import s from './MoviePage.module.css';
+import s from "./MoviePage.module.css";
 import VideoSearch from "../../components/videoSearch/VideoSearch";
 import MovieList from "../../components/MoviesList/MovieList";
 import MoviesFilter from "../../components/MoviesFilter/MoviesFilter";
 
-
 const MoviePage = () => {
-  
   return (
     <div className={s.moviePage}>
       <Navbar />
-
-      
       <div className={s.movieContent}>
-      <MoviesFilter />
-      <VideoSearch />
+        <MoviesFilter />
+        <VideoSearch />
       </div>
       <MovieList />
     </div>
-  )
-}
+  );
+};
 
-export default MoviePage
+export default MoviePage;
